70c44dccb00125f50c0856e48ea4d40fe388092d
pbcpraveen <pbcpraveen@gmail.com>
2023-09-26 11:08:04 -0700

    Fixing issues with link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
+import React, { Suspense, lazy } from 'react';
 import ReadingList from "./pages/readingList";
 import Presentations from "./pages/presentations";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from "./utils/navbar";
 function App() {
+    const NotFound = lazy(() => import('./pages/notfound'));
   return (
        <>
            <Router>
-              <Navbar />
-              <Routes>
-                  <Route exact path='/'  element={<Presentations />} />
-                  <Route path='/readinglist' element={<ReadingList />} />
-                  <Route path='/presentation' element={<Presentations />} />
-              </Routes>
+               <Suspense fallback={<Presentations />}>
+                  <Navbar />
+                  <Routes>
+
+                          <Route exact path='/'  element={<Presentations/>} />
+                          <Route path='/readinglist' element={<ReadingList/>} />
+                          <Route path='/presentation' element={<Presentations/>} />
+                          <Route component={NotFound} status={404} />
+
+                  </Routes>
+               </Suspense>
           </Router>
       </>
 
